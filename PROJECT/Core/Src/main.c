4f59cdc7befbdcd0a8c1f7cc50fd28bc88637c7a
Hồ Ngọc Anh Tuấn <tuan.hona2004@hcmut.edu.vn>
2024-09-24 17:03:48 +0700

    no message

--- PROJECT/Core/Src/main.c
+++ PROJECT/Core/Src/main.c
@@ -107,8 +107,8 @@ int main(void)
 		  case 0:
 			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, SET);
 			  HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, SET);
 			  HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, RESET);
 			  HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, RESET);
@@ -118,8 +118,10 @@ int main(void)
 			  HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, SET);
 			  break;
 		  case 1:
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN0_Pin, SET);
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN1_Pin, RESET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, SET);
 			  HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, RESET);
 			  HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, RESET);
 			  HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, SET);
@@ -129,26 +131,30 @@ int main(void)
 			  HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, RESET);
 			  break;
 		  case 2:
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
-			  HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, SET);
-			  HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, RESET);
-			  HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, RESET);
-			  HAL_GPIO_WritePin(SEG3_GPIO_Port, SEG3_Pin, SET);
-			  HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, SET);
-			  HAL_GPIO_WritePin(SEG5_GPIO_Port, SEG5_Pin, SET);
-			  HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, SET);
-			  break;
-		  case 3:
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN0_Pin, SET);
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN1_Pin, RESET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, RESET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, SET);
 			  HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, RESET);
 			  HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, RESET);
-			  HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, SET);
+			  HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, RESET);
 			  HAL_GPIO_WritePin(SEG3_GPIO_Port, SEG3_Pin, RESET);
-			  HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, SET);
 			  HAL_GPIO_WritePin(SEG5_GPIO_Port, SEG5_Pin, SET);
 			  HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, RESET);
+			  break;
+		  case 3:
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN2_Pin, SET);
+			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG3_GPIO_Port, SEG3_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG5_GPIO_Port, SEG5_Pin, RESET);
+			  HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, SET);
 			  break;
 
 	  }
