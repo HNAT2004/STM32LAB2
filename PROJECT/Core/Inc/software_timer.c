2aa74ee8954fd34e4ba61e07656c995980e4f867
Hồ Ngọc Anh Tuấn <tuan.hona2004@hcmut.edu.vn>
2024-09-26 10:22:12 +0700

    no message

--- PROJECT/Core/Inc/software_timer.c
+++ PROJECT/Core/Inc/software_timer.c
@@ -195,5 +195,32 @@ void updateClockBuffer(void){
 }
 
 void updateLEDMatrix(int index){
+	switch(index){
+	case 0:
 
+		break;
+	case 1:
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+	case 4:
+
+		break;
+	case 5:
+
+		break;
+	case 6:
+
+		break;
+	case 7:
+
+		break;
+	default:
+		break;
+	}
 }
