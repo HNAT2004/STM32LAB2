ceebf9e755e898d4720bd620de72e516e439be23
Hồ Ngọc Anh Tuấn <tuan.hona2004@hcmut.edu.vn>
2024-09-27 20:41:46 +0700

    no message

--- PROJECT/Core/Inc/software_timer.c
+++ PROJECT/Core/Inc/software_timer.c
@@ -213,37 +213,6 @@ void updateLEDMatrix(int index){
 	}
 }
 
-void updateLEDMatrix_2(int index){
-	switch(index){
-	case 0:
-		HAL_GPIO_WritePin(ROW0_GPIO_Port, ROW0_Pin, RESET);
-		break;
-	case 1:
-		HAL_GPIO_WritePin(ROW1_GPIO_Port, ROW1_Pin, RESET);
-		break;
-	case 2:
-		HAL_GPIO_WritePin(ROW2_GPIO_Port, ROW2_Pin, RESET);
-		break;
-	case 3:
-		HAL_GPIO_WritePin(ROW3_GPIO_Port, ROW3_Pin, RESET);
-		break;
-	case 4:
-		HAL_GPIO_WritePin(ROW4_GPIO_Port, ROW4_Pin, RESET);
-		break;
-	case 5:
-		HAL_GPIO_WritePin(ROW5_GPIO_Port, ROW5_Pin, RESET);
-		break;
-	case 6:
-		HAL_GPIO_WritePin(ROW6_GPIO_Port, ROW6_Pin, RESET);
-		break;
-	case 7:
-		HAL_GPIO_WritePin(ROW7_GPIO_Port, ROW7_Pin, RESET);
-		break;
-	default:
-		break;
-	}
-}
-
 void displayA(int index) {
 	updateLEDMatrix(index);
     uint8_t row_signal = matrix_buffer[index];
